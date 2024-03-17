c5f1b5992fe9e36708146ecffdb191c7e3043767
Fajri009 <102470811+Fajri009@users.noreply.github.com>
2024-03-17 12:28:11 +0700

    Start

---
+++ app/src/main/java/com/example/bangkit_2024_ip_bfaa/ui/searchuserpage/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.bangkit_2024_ip_bfaa.ui.searchuserpage
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bangkit_2024_ip_bfaa.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
